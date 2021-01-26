01b6980814d796938cd864e09449ec55134c64f2
Tina Ostrander <tostrander@greenriver.edu>
2021-01-26 15:49:08 -0500

    Added additional routes

--- index.php
+++ index.php
@@ -19,11 +19,58 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+//Define a default route (home page)
+$f3->route('GET /@first/@last', function($f3, $params) {
+    echo "Hello, ".$params['first']." ".$params['last'];
+});
+
 //Define a "breakfast" route
 $f3->route('GET /breakfast', function() {
     //echo "Breakfast";
     $view = new Template();
     echo $view->render('views/breakfast.html');
+});
+
+//Define a "lunch" route
+$f3->route('GET /lunch', function() {
+
+    $view = new Template();
+    echo $view->render('views/lunch.html');
+});
+
+//Define a "lunch/sandwich" route
+$f3->route('GET /lunch/sandwiches', function() {
+
+    $view = new Template();
+    echo $view->render('views/sandwich.html');
+});
+
+$f3->route('GET /breakfast/@item', function($f3, $params) {
+
+    var_dump($params);
+    $menu = array('eggs', 'waffles', 'pancakes');
+    $item = $params['item'];
+    if (in_array($item, $menu)) {
+        switch($item) {
+            case 'eggs':
+                $view = new Template();
+                echo $view->render('views/eggs.html');
+                break;
+            case 'pancakes':
+                echo "Swedish or American?";
+                break;
+            case 'waffles':
+                $f3->reroute("http://thewafflehouse.com");
+                break;
+            default:
+                $f3->error(404);
+        }
+    }
+    else {
+        echo "Sorry, we don't serve $item";
+    }
+    //$view = new Template();
+    //echo $view->render('views/breakfast.html');
 });
 
 //Run fat free
